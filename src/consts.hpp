2349e81058151feb19ac5dadec96bfe80bdd9e8b
Michael Nath <michael.d.nath@gmail.com>
2024-02-18 16:40:05 -0800

    baseline reward sys seems to be buggy?

--- src/consts.hpp
+++ src/consts.hpp
@@ -8,7 +8,6 @@ namespace consts {
 // Each random world is composed of a fixed number of rooms that the agents
 // must solve in order to maximize their reward.
 inline constexpr madrona::CountT numRooms = 3;
-// inline constexpr madrona::CountT numRooms = 1; 
 
 // Generated levels assume 2 agents
 inline constexpr madrona::CountT numAgents = 2;
@@ -26,17 +25,10 @@ inline constexpr float agentRadius = 1.f;
 inline constexpr float roomLength = worldLength / numRooms;
 
 // Each unit of distance forward (+ y axis) rewards the agents by this amount
-inline constexpr float rewardPerDist = 0.1f;
-
-// Reward for button press
-inline constexpr float buttonReward = 0.1f; 
-// Reward for pressing all buttons
-inline constexpr float rewardPerAllButtons = 0.25f;
-
+inline constexpr float rewardPerDist = 0.05f;
 // Each step that the agents don't make additional progress they get a small
-// penalty reward (progress towards button or in y direction)
-inline constexpr float slackReward = -0.05f;
-inline constexpr float slackReward2 = -0.005f;
+// penalty reward
+inline constexpr float slackReward = -0.005f;
 
 // Steps per episode
 inline constexpr int32_t episodeLen = 200;
@@ -47,7 +39,7 @@ inline constexpr madrona::CountT numMoveAngleBuckets = 8;
 inline constexpr madrona::CountT numTurnBuckets = 5;
 
 // Number of lidar samples, arranged in circle around agent
-inline constexpr madrona::CountT numLidarSamples = 1;
+inline constexpr madrona::CountT numLidarSamples = 30;
 
 // Time (seconds) per step
 inline constexpr float deltaT = 0.04f;
--- src/sim.cpp
+++ src/sim.cpp
@@ -454,8 +454,6 @@ inline void collectObservationsSystem(Engine &ctx,
         } else {
             Vector3 entity_pos = ctx.get<Position>(entity);
             EntityType entity_type = ctx.get<EntityType>(entity);
-            if (entity_type == EntityType::Agent)
-                printf("%d: %d\n", i, entity_type);
 
             Vector3 to_entity = entity_pos - pos;
             
@@ -593,91 +591,27 @@ inline void resetProgressSystem(Engine &ctx,
 // Computes reward for each agent and keeps track of the max distance achieved
 // so far through the challenge. Continuous reward is provided for any new
 // distance achieved.
-inline void rewardSystem(Engine &ctx,
+inline void rewardSystem(Engine &,
                          Position pos,
                          Progress &progress,
-                         SelfObservation obs,
                          Reward &out_reward)
 {
-    float reward_pos_y = fminf(pos.y, consts::worldLength * 2);
-    float reward_pos_x; 
-    if (pos.x < 0) {
-        reward_pos_x = fmaxf(pos.x, -consts::worldWidth);
-    }
-    else {
-        reward_pos_x = fminf(pos.x, consts::worldWidth);
-    }
+    // Just in case agents do something crazy, clamp total reward
+    float reward_pos = fminf(pos.y, consts::worldLength * 2);
 
-    int room_idx = progress.maxY / (consts::roomLength * 1.025f);
+    float old_max_y = progress.maxY;
 
-    LevelState &level = ctx.singleton<LevelState>();
-    Entity button = level.rooms[room_idx].entities[progress.button_id];
+    float new_progress = reward_pos - old_max_y;
 
-    float b_x = ctx.get<Position>(button).x;
-    float b_y = ctx.get<Position>(button).y;
-
-    float dx = reward_pos_x - b_x; 
-    float dy = reward_pos_y - b_y; 
-
-    // incentivise agents to always look forward
-
-
-    if (!progress.pressedAllButtons) {
-        // check to see if button is now pressed
-        if (ctx.get<ButtonState>(button).isPressed) {
-            out_reward.v = consts::buttonReward;
-            progress.pressedButton = true;
-        }
-        else {
-            float cur_dist = sqrtf(dx * dx + dy * dy);
-            if (cur_dist >= progress.initialDist) {
-                // penalize for not making progress
-                out_reward.v = consts::slackReward;
-            }
-            else {
-                // reward for making progress
-                out_reward.v = fminf(consts::buttonReward - 0.01f, expf(-1.0f * (2 + cur_dist))); 
-                progress.initialDist = cur_dist;
-            }
-        }
-    }
-    else {
-        float new_progress_y = reward_pos_y - progress.maxY;
-        if (new_progress_y > 0) {
-            // reward for maximizing y 
-            out_reward.v = new_progress_y * consts::rewardPerDist;
-            progress.maxY = reward_pos_y;
-        }
-        else {
-            out_reward.v = consts::slackReward2;
-        }
-    }
-}
-
-inline void doorRewardSystem(Engine &ctx,
-                            Progress &progress,
-                            Reward &reward)
-{
-    // count how many buttons are pressed
-    int32_t num_pressed = 0;
-    int     current_rm  = progress.cur_room_idx;
-
-    LevelState &level = ctx.singleton<LevelState>();
-    DoorProperties props = ctx.get<DoorProperties>(level.rooms[current_rm].door);
-
-    for (int32_t i = 0; i < props.numButtons; i++) {
-        Entity button = props.buttons[i];
-        ButtonState button_state = ctx.get<ButtonState>(button);
-        if (button_state.isPressed) {
-            num_pressed++;
-        }
+    float reward;
+    if (new_progress > 0) {
+        reward = new_progress * consts::rewardPerDist;
+        progress.maxY = reward_pos;
+    } else {
+        reward = consts::slackReward;
     }
 
-    // give reward if all buttons are pressed
-    if (!progress.pressedAllButtons && num_pressed == props.numButtons) {
-        reward.v += consts::rewardPerAllButtons;
-        progress.pressedAllButtons = true;
-    }
+    out_reward.v = reward;
 }
 
 // Each agent gets a small bonus to it's reward if the other agent has
@@ -689,8 +623,6 @@ inline void bonusRewardSystem(Engine &ctx,
                               Progress &progress,
                               Reward &reward)
 {
-    // determining if partners are close to each other
-    // or not
     bool partners_close = true;
     for (CountT i = 0; i < consts::numAgents - 1; i++) {
         Entity other = others.e[i];
@@ -701,7 +633,6 @@ inline void bonusRewardSystem(Engine &ctx,
         }
     }
 
-    // if yes, then give a bonus reward
     if (partners_close && reward.v > 0.f) {
         reward.v *= 1.25f;
     }
@@ -805,41 +736,28 @@ void Sim::setupTasks(TaskGraphBuilder &builder, const Config &cfg)
             DoorProperties
         >>({button_sys});
 
-    auto reset_progress_sys = builder.addToGraph<ParallelForNode<Engine,
-        resetProgressSystem,
-            Position,
-            Progress
-        >>({door_open_sys});
-
     // Compute initial reward now that physics has updated the world state
     auto reward_sys = builder.addToGraph<ParallelForNode<Engine,
          rewardSystem,
             Position,
             Progress,
-            SelfObservation,
             Reward
-        >>({reset_progress_sys});
-    
-    auto door_reward_sys = builder.addToGraph<ParallelForNode<Engine,
-         doorRewardSystem,
+        >>({door_open_sys});
+
+    // Assign partner's reward
+    auto bonus_reward_sys = builder.addToGraph<ParallelForNode<Engine,
+         bonusRewardSystem,
+            OtherAgents,
             Progress,
             Reward
         >>({reward_sys});
-
-    // Assign partner's reward
-    // auto bonus_reward_sys = builder.addToGraph<ParallelForNode<Engine,
-    //      bonusRewardSystem,
-    //         OtherAgents,
-    //         Progress,
-    //         Reward
-    //     >>({door_reward_sys});
 
     // Check if the episode is over
     auto done_sys = builder.addToGraph<ParallelForNode<Engine,
         stepTrackerSystem,
             StepsRemaining,
             Done
-        >>({door_reward_sys});
+        >>({bonus_reward_sys});
 
     // Conditionally reset the world if the episode is over
     auto reset_sys = builder.addToGraph<ParallelForNode<Engine,
@@ -917,7 +835,6 @@ void Sim::setupTasks(TaskGraphBuilder &builder, const Config &cfg)
     (void)collect_obs;
 #endif
 }
-
 
 Sim::Sim(Engine &ctx,
          const Config &cfg,
